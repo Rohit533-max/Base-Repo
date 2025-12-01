62c6d5f218c966e3d6d86530d3a4ac1cf95033c5
Rohit533-max <rohit03383@gmail.com>
2025-12-01 12:31:59 +0530

    Setting up the second matrix

--- Matrix.py
+++ Matrix.py
@@ -6,7 +6,7 @@ col = int(input("Enter the number of columns: "))
 
 #Initializing an empty matrix for storing the values
 matrix = []
-
+matrix2=[]
 #Take input row by row
 for i in range(row):
     row =[]
@@ -15,7 +15,20 @@ for i in range(row):
         row.append(element)
     matrix.append(row)
 
-#converting the matrix into an array
+#converting the matrix into an array for first matrix
 arr= np.array(matrix)
+print()
+print("Enter the sencond matrix ->")
+row2 = int(input("Enter the number of rows: "))
+col2 = int(input("Enter the number of columns: "))
+#Take input for the second matrix
+for i in range(row2):
+    row2 =[]
+    for j in range(col2):
+        element = int(input(f"Enter the row{i+1},column{j+1} element: "))
+        row.append(element)
+    matrix.append(row2)
 
-print(arr)
+#converting the matrix into an array for second martix
+
+arr2 = np.array(matrix2)
