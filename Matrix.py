ac5e1c2b6dd98b8bccb859103101bca30e008dba
Rohit533-max <rohit03383@gmail.com>
2025-12-02 22:27:50 +0530

    Working on logics

--- Matrix.py
+++ Matrix.py
@@ -2,3 +2,17 @@ import tkinter as tk
 from tkinter import messagebox
 import numpy as np
 
+#Step 1: -> Writing the logic of the program
+def add(a,b):
+    return a +b
+def subs(a,b):
+    return a - b
+def dot(a,b):
+    return a.b
+def inverse(a,b):
+    return #Logic is pending...
+def deter(a,b):
+    return #logic pending...
+
+root = tk.Tk()
+root.mainloop()
