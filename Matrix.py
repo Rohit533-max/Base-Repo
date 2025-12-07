2d7c72b770c55a31cdfc3b9d1e44a79653e827fc
Rohit533-max <rohit03383@gmail.com>
2025-12-07 12:45:33 +0530

    Setting up the matrix block

--- Matrix.py
+++ Matrix.py
@@ -23,23 +23,127 @@ matrix = []
 # Create main window
 root = tk.Tk()
 root.title("Number Input Block")
-root.geometry("300x150")
+root.geometry("500x250")
 
+label = tk.Label(root,text="Matrix 1")
+label.place(x=190,y=30)
 # Label
-label = tk.Label(root, text="Enter a number:")
-label.pack(pady=10)
+label = tk.Label(root, text="Enter a number:",)
+label.place(x=100,y=50)
 
 # Entry widget (block to write number)
 entry = tk.Entry(root, width=10)
-entry.pack(pady=5)
-
-#Label2
-label2 = tk.Label(root, text = "Enter num2")
-label2.pack(pady = 10)
+entry.place(x=110,y=70)
 
 #Entry widget2
-entry2 = tk.Entry(root, width=100)
-entry.pack(pady=5)
+entry2 = tk.Entry(root, width=10)
+entry2.place(x=110,y=100)
+
+#Entry widget3
+entry3 = tk.Entry(root, width="10")
+entry3.place(x=110,y=130)
+
+#second column
+
+# Entry widget (block to write number)
+entry = tk.Entry(root, width=10)
+entry.place(x=190,y=70)
+
+
+# Entry widget (block to write number)
+entry = tk.Entry(root, width=10)
+entry.place(x=190,y=100)
+
+
+# Entry widget (block to write number)
+entry = tk.Entry(root, width=10)
+entry.place(x=190,y=130)
+
+
+#third column
+# Entry widget (block to write number)
+entry = tk.Entry(root, width=10)
+entry.place(x=270,y=70)
+
+
+# Entry widget (block to write number)
+entry = tk.Entry(root, width=10)
+entry.place(x=270,y=100)
+
+
+# Entry widget (block to write number)
+entry = tk.Entry(root, width=10)
+entry.place(x=270,y=130)
+
+#Second matrix
+label = tk.Label(root,text="Matrix 1")
+label.place(x=190,y=30)
+# Label
+label = tk.Label(root, text="Enter a number:",)
+label.place(x=100,y=50)
+
+# Entry widget (block to write number)
+entry = tk.Entry(root, width=10)
+entry.place(x=110,y=70)
+
+#Entry widget2
+entry2 = tk.Entry(root, width=10)
+entry2.place(x=110,y=100)
+
+#Entry widget3
+entry3 = tk.Entry(root, width="10")
+entry3.place(x=110,y=130)
+
+#second matrix---------------------------------------------------------------------------------------------------------------------------------------
+
+label = tk.Label(root,text="Matrix 2")
+label.place(x=450,y=30)
+
+# Entry widget (block to write number)
+entry = tk.Entry(root, width=10)
+entry.place(x=390,y=70)
+
+
+# Entry widget (block to write number)
+entry = tk.Entry(root, width=10)
+entry.place(x=390,y=100)
+
+
+# Entry widget (block to write number)
+entry = tk.Entry(root, width=10)
+entry.place(x=390,y=130)
+
+
+#third column
+# Entry widget (block to write number)
+entry = tk.Entry(root, width=10)
+entry.place(x=470,y=70)
+
+
+# Entry widget (block to write number)
+entry = tk.Entry(root, width=10)
+entry.place(x=470,y=100)
+
+
+# Entry widget (block to write number)
+entry = tk.Entry(root, width=10)
+entry.place(x=550,y=130)
+
+
+#second column
+# Entry widget (block to write number)
+entry = tk.Entry(root, width=10)
+entry.place(x=550,y=70)
+
+
+# Entry widget (block to write number)
+entry = tk.Entry(root, width=10)
+entry.place(x=550,y=100)
+
+
+# Entry widget (block to write number)
+entry = tk.Entry(root, width=10)
+entry.place(x=470,y=130)
 
 # Function to read the number
 def get_number():
@@ -56,5 +160,8 @@ result_label.pack(pady =5)
 
 result_label2 = tk.Label(root, text ="")
 result_label2.pack(pady =5)
+
+result_label3 = tk.Label(root, text="")
+result_label3.pack(pady=5)
 
 root.mainloop()
