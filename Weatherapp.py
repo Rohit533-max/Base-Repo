8c30a2d2cfa96824ab426d8a51251658e6c2cf79
Rohit533-max <rohit03383@gmail.com>
2025-12-10 21:07:51 +0530

    Weather labels adjustment

--- Weatherapp.py
+++ Weatherapp.py
@@ -6,7 +6,7 @@ from tkinter import ttk
 root = Tk()
 
 root.title("weather App")
-root.geometry("400x400")
+root.geometry("600x600")
 
 root.config(bg="blue")
 
@@ -45,9 +45,35 @@ list_name = [
     "Uttarakhand",
     "West Bengal"
 ]
-com = ttk.Combobox(root,text = "", font=("Time New Roman", 10, "italic"), values= list_name)
+#values in combobox is a build in func that stores the values that are gonna repersent in the options
+com = ttk.Combobox(root,text = "", font=("Time New Roman", 12, "italic"), values= list_name)
 com.place(x=200,y=190, width =250, height = 30)
 
-#values in combobox is a build in func that stores the values that are gonna repersent in the options
+#submit button
+sub_button = Button(root, text = "Submit", font=("Arial", 10))
+sub_button.place(x=300, y=250)
+
+
+#Weather label
+
+#creating the mirroring labels to show the data after fetching
+
+w_label = Label(root, text="Weather Update", font=("Time New Roman", 20, "bold", "italic"))
+w_label.place(x=120, y= 300)
+
+wd_label = Label(root, text = "Weather Description", font = ("Time New Roman", 10))
+wd_label.place(x=120, y=350)
+
+temp = Label(root, text = "Temperature: ", font = ("Time New Roman", 10))
+temp.place(x=120, y=400)
+
+temp1 = Label(root, text = "T", font = ("Time New Roman", 10))
+temp1.place(x=240, y=400)
+
+pre = Label(root, text = "Pressure: ", font = ("Time New Roman", 10))
+pre.place(x=120, y=450)
+
+pre1= Label(root, text = "P", font = ("Time New Roman", 10))
+pre1.place(x=240, y=450)
 
 root.mainloop()
